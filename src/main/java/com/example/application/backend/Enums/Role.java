179bf1abac7020ac8375d423ee0abe28e53d14d5
Rosu Andrei <andrei.rosu@tremend.com>
2023-04-25 16:08:45 +0300

    Dev-1-Created the User class and the Role enum. Created the OneToOne relationship between Person and User using the shared primary key method. Added in the User class methods that hash the password of the user and that also makes it unique usingh the passwordSalt.

---
+++ src/main/java/com/example/application/backend/Enums/Role.java
@@ -0,0 +1,6 @@
+package com.example.application.backend.Enums;
+
+public enum Role {
+
+    USER, ADMIN
+}
---
+++ src/main/java/com/example/application/backend/Model/User.java
@@ -0,0 +1,47 @@
+package com.example.application.backend.Model;
+
+import com.example.application.backend.Enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.RandomStringUtils;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "person_id")
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    private String passwordSalt;
+
+    private String passwordHash;
+
+    @Column(name = "role")
+    private Role role;
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "person_id")
+    private Person person;
+
+    public User(String username, String password, Role role) {
+        this.username = username;
+        this.role = role;
+        this.passwordSalt = RandomStringUtils.random(32);
+        this.passwordHash = DigestUtils.sha1Hex(password + passwordSalt); // the append is done so that if 2
+    }                       // users have the same hash they still will have a different password because of the passwordSalt
+
+    public boolean checkPassword(String password) {
+        return DigestUtils.sha1Hex(password + passwordSalt).equals(passwordHash);
+    }
+
+}
