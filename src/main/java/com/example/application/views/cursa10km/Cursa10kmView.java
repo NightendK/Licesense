392744488411b87f8340c3e30a354ba93bc82174
Rosu Andrei <andrei_rosu1986@yahoo.com>
2023-06-13 17:33:25 +0300

    master-> Completed the race views with informations and map for each.

--- src/main/java/com/example/application/views/cursa10km/Cursa10kmView.java
+++ src/main/java/com/example/application/views/cursa10km/Cursa10kmView.java
@@ -1,8 +1,10 @@
 package com.example.application.views.cursa10km;
 
 import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
@@ -10,25 +12,46 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
 
 @PageTitle("Cursa 10km")
+@CssImport("./styles/views/cursa42/cursa42-view.css")
 //@Route(value = "10km", layout = MainLayout.class)
 public class Cursa10kmView extends VerticalLayout {
+    private Label date = new Label();
+    private Label startTime = new Label();
+    private Label distance = new Label();
+    private Label limitTime = new Label();
+    private Label startLocation = new Label();
+    private Image raceImage = new Image("images/Cursa10.png", "race");
 
     public Cursa10kmView() {
-        setSpacing(false);
-
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
-
-        H2 header = new H2("This place intentionally left empty");
-        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
-        add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
 
         setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+        setSpacing(true);
+
+        date.setId("date");
+        date.setText("Date : 27 september 2023");
+
+        startTime.setId("startTime");
+        startTime.setText("Start time : 09:00");
+
+        distance.setId("distance");
+        distance.setText("Distance : 10Km");
+
+        limitTime.setId("limitTime");
+        limitTime.setText("The race must be completed in maximum 2 hours. Candidates who complete the race in a time greater than 2 hours " +
+                "won't be displayed on the time board.");
+
+        startLocation.setId("startLoc");
+        startLocation.setText("Start location: Piața Presei Libere 1, București\n\n");
+        add(date, startTime, startLocation, distance, limitTime);
+
+        Label title = new Label();
+        title.setId("raceMap");
+        title.setText("\n\nRace Map");
+
+        add(title);
+
+        raceImage.setId("raceImage");
+        add(raceImage);
     }
 
 }
--- src/main/java/com/example/application/views/cursa42km/Cursa42KmView.java
+++ src/main/java/com/example/application/views/cursa42km/Cursa42KmView.java
@@ -1,42 +1,56 @@
 package com.example.application.views.cursa42km;
 
-import com.example.application.views.MainLayout;
-import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
+
 
 @PageTitle("Cursa 42Km")
+@CssImport("./styles/views/cursa42/cursa42-view.css")
 //@Route(value = "42km", layout = MainLayout.class)
 public class Cursa42KmView extends VerticalLayout {
 
+    private Label date = new Label();
+    private Label startTime = new Label();
+    private Label distance = new Label();
+    private Label limitTime = new Label();
+    private Label startLocation = new Label();
+    private Image raceImage = new Image("images/Cursa42.png", "race");
+
     public Cursa42KmView() {
-        /*setSpacing(false);
-
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
-
-        H2 header = new H2("This place intentionally left empty");
-        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
-        add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
 
         setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");*/
+        setSpacing(true);
 
-        add(new Paragraph("This is the first paragraph introduces as a test for " +
-                "future implementations. Let's hope it works as i expect.\n " +
-                "\nLet's see if this also creates a new line xD"));  // only the new line doesn't work
+        date.setId("date");
+        date.setText("Date : 24 september 2023");
 
-        Image img = new Image("images/run.jpg", "run");
-        img.setWidth("200px");
-        add(img);
+        startTime.setId("startTime");
+        startTime.setText("Start time : 10:00");
+
+        distance.setId("distance");
+        distance.setText("Distance : 42Km");
+
+        limitTime.setId("limitTime");
+        limitTime.setText("The race must be completed in maximum 6 hours. Candidates who complete the race in a time greater than 6 hours " +
+                "won't be displayed on the time board.");
+
+        startLocation.setId("startLoc");
+        startLocation.setText("Start location: Piața Presei Libere 1, București\n\n");
+        add(date, startTime, startLocation, distance, limitTime);
+
+        Label title = new Label();
+        title.setId("raceMap");
+        title.setText("\n\nRace Map");
+
+        add(title);
+
+        raceImage.setId("raceImage");
+        add(raceImage);
     }
+
 
 }
