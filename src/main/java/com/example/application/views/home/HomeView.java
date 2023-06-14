dd49cd9f6fc3c1e7824eaf7df39d8daaa80dfbbb
Rosu Andrei <andrei_rosu1986@yahoo.com>
2023-06-14 16:20:22 +0300

    Dev_10 -> Added logo and text to the home View.

--- src/main/java/com/example/application/views/home/HomeView.java
+++ src/main/java/com/example/application/views/home/HomeView.java
@@ -3,8 +3,12 @@ package com.example.application.views.home;
 import com.example.application.views.MainLayout;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -13,24 +17,38 @@ import jakarta.annotation.security.DenyAll;
 import jakarta.annotation.security.PermitAll;
 
 @PageTitle("Home")
-//@Route(value = "home", layout = MainLayout.class)
-public class HomeView extends HorizontalLayout {
+@CssImport("./styles/views/home/home-view.css")
+public class HomeView extends VerticalLayout {
 
-    private TextField name;
-    private Button sayHello;
+    private Image raceLogo = new Image();
 
     public HomeView() {
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
-        sayHello.addClickShortcut(Key.ENTER);
 
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+        setSizeFull();
+        setSpacing(true);
+        raceLogo.setSrc("images/RaceLogoV2.png");
+        raceLogo.setId("raceLogo");
+        add(raceLogo);
 
-        add(name, sayHello);
+        Label text = new Label();
+        text.setId("text");
+        text.setText("We are pleased to welcome you to the first edition of the Road Runner Race. This year's competition" +
+                " will take place in Bucharest, capital of Romania");
+
+        Label text2 = new Label();
+        text2.setId("text");
+        text2.setText("The competition is divided into 4 races: 42Km race, 21Km race, 10Km race and a race " +
+                "for the children, 1Km race. Our objective is to offer the chance for any individual to participate into a physical activity outside.");
+
+        Label text3 = new Label();
+        text3.setId("text");
+        text3.setText("Road Runner Race (R.R.R): We are an organization that support physical activity, especially running as a means " +
+                "of meditation and stress relief. Our goal is to organise races in every capital in the European Union so we can promote" +
+                " physical activity in our increasing sedentary lifestyle. The profits we make will be donated to charity and to research into how" + "" +
+                " running can reduce our daily life stress.");
+
+
+        add(text, text2, text3);
     }
 
 }
